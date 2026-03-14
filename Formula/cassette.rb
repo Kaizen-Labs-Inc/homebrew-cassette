6b1e436c0131acbab2a28f25be695a2dfdbb6e50
Jordan Fulghum <jordan@fulghum.io>
2026-03-14 07:33:32 -0400

    Point cassette formula at org release URL

--- Formula/cassette.rb
+++ Formula/cassette.rb
@@ -1,7 +1,7 @@
 class Cassette < Formula
   desc "Cassette command-line client"
   homepage "https://cassette.sh"
-  url "https://github.com/jordanful/cassette/releases/download/cassette-cli-v0.1.0/cassette-cli-0.1.0.gem"
+  url "https://github.com/Kaizen-Labs-Inc/cassette/releases/download/cassette-cli-v0.1.0/cassette-cli-0.1.0.gem"
   sha256 "b2d32868938f5da5c27e43a18971b540f24673c2ea3953929b85516e4fec949e"
   license "MIT"
 
